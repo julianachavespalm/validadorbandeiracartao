3202c97aebae2bb301b34479d8c990e49b50d179
Juliana Chaves Palm <juliana@example.com>
2026-04-19 16:37:04 -0300

    Adiciona arquivos de configuração e utilitários de asserção

---
+++ src/test/java/io/github/julianachavespalm/validadorbandeiracartao/utils/AssertUtils.java
@@ -0,0 +1,18 @@
+package io.github.julianachavespalm.validadorbandeiracartao.utils;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.visible;
+
+public class AssertUtils {
+    public static void deveEstarVisivelETerTextoExato(SelenideElement elemento, String texto) {
+        elemento.shouldBe(visible)
+                .shouldHave(Condition.exactText(texto));
+    }
+
+    public static void deveEstarVisivelETerAtributoComValor(SelenideElement elemento, String atributo, String valor) {
+        elemento.shouldBe(visible)
+                .shouldHave(Condition.attribute(atributo, valor));
+    }
+}
